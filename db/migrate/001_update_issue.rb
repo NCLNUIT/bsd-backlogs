cd170ef71496396755e29901941f8ea8ea448a53
alex <alex@a-graham.com>
2015-03-25 15:53:50 +0000

    Now adds self to all trackers on install

--- db/migrate/001_update_issue.rb
+++ db/migrate/001_update_issue.rb
@@ -1,8 +1,9 @@
 class UpdateIssue < ActiveRecord::Migration
     def self.up
         if CustomField.find_by_name('Backlog').nil?
-            IssueCustomField.create(name: 'Backlog', field_format: 'int', is_for_all: 1, is_filter: 1)
-            ##.trackers = Tracker.find(:all)
+            cf = IssueCustomField.create(name: 'Backlog', field_format: 'int', is_for_all: 1, is_filter: 1)
+            cf.trackers = Tracker.find(:all)
+            cf.save
         end
     end
 
